7e7a251f605d40aeec5c5a01275df4685aab8b81
carogn <carolina.gonzalez070602@gmail.com>
2026-06-26 19:40:29 -0300

    router get Comment by id

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -1,25 +1,26 @@
-// Importo express y creo un router para definir las rutas de postImage
-const express = require("express")
-const router = express.Router()
+// Importo express y creo un router para definir las rutas de comentarios
+const express = require("express");
+const router = express.Router();
 
-// Destrocturo los metodos del controlador para usarlos en las rutas
+// Desestructuro los métodos del controlador para usarlos en las rutas
 const {
     getComments,
+    getCommentById,
     createComment,
     updateComment,
     deleteComment
-} = require("../controllers/commentController")
+} = require("../controllers/commentController");
 
+// Importo el middleware de validación de esquemas y el esquema de comentario
+const { schemaValidator } = require("../middlewares/validateSchema");
+const { commentSchema } = require("../validations/commentSchema");
 
-// Importo el middleware de validación de esquemas y el esquema de usuario para validar los datos de entrada
-const { schemaValidator } = require('../middlewares/validateSchema')
-const { commentSchema } = require("../validations/commentSchema")
-
-// Defino las rutas y les asigno el metodo correspondiente del controlador con validaciones donde corresponda
-router.get("/", getComments)
-router.post("/", schemaValidator(commentSchema), createComment)
-router.put("/:id", schemaValidator(commentSchema), updateComment)
-router.delete("/:id", deleteComment)
+// Defino las rutas y les asigno el método correspondiente del controlador
+router.get("/", getComments);
+router.get("/:id", getCommentById);
+router.post("/", schemaValidator(commentSchema), createComment);
+router.put("/:id", schemaValidator(commentSchema), updateComment);
+router.delete("/:id", deleteComment);
 
 // Exporto el router para usarlo en main.js
-module.exports = router
+module.exports = router;
